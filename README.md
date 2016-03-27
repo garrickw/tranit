06e24ea68a0986d1666b583a37043eef23655c4d
garrick <wzsgarrick@gmail.com>
2016-03-27 09:31:10 +0800

    do some formatting

--- README.md
+++ README.md
@@ -1,43 +1,54 @@
+#Tranit
+
 This a small tool for translating. Base on the command line and youdao API.
 Word or sentence are both excepted.
 English or Chinese are both excepted.
 
-usage example:
+###set up:
+1.apply for a youdao api key and save in a flie which contains:
+>keyfrom:(your keyfrom)
+>key:(youkey)
+2.change the KEYFILE(path)  pointing to your own keyfile
 
-./tranit.py cool
 
-result===>
+###usage example:
 
-#很酷的
-#--------------------
-#n. 凉爽；凉爽的空气
-#adj. 凉爽的；冷静的；出色的
-#vt. 使…冷却；使…平静下来
-#vi. 变凉；平息
-#adv. 冷静地
-#n. (Cool)人名；(法)科尔；(英)库尔
-#--------------------
-#From web:
-#cool:
-#凉爽的; 酷; 酷
-#Cool jazz:
-#冷爵士乐; 冷爵士; 冷爵士乐
-#3Tumble Cool:
-#用烘干机低温烘干; 打冷风; 用烘焙机低温烘焙
+`./tranit.py cool`
+>cool
+>很酷的
+>--------------------
+>kuːl                                                                                                                                                                    
+>us: kul                                                                                                                                                                 
+>uk: kuːl                                                                                                                                                                
+>--------------------                                                                                                                                                    
+>n. 凉爽；凉爽的空气                                                                                                                                                     
+>adj. 凉爽的；冷静的；出色的
+>vt. 使…冷却；使…平静下来
+>vi. 变凉；平息
+>adv. 冷静地
+>n. (Cool)人名；(法)科尔；(英)库尔
+>--------------------
+>From web:
+>cool:
+>凉爽的; 酷; 酷
+>Cool jazz:
+>冷爵士乐; 冷爵士; 冷爵士乐
+>Tumble Cool:
+>用烘干机低温烘干; 打冷风; 用烘焙机低温烘焙
 
-./tranit.py 天气
 
-result===>
+`./tranit.py 做运动`
 
-#weather
-#--------------------
-#[气象] weather
-#--------------------
-#From web:
-#天气:
-#Weather; weather; Temps
-#天气预报:
-#Weather Forecast; weather forecast; 天気予報
-#天气谚语:
-#weather lore; 観天望気; Weather lore
+>做运动
+>Do sports
+>--------------------
+>zuò yùn dòng
+>From web:
+>做运动:
+>play sports; do sports; take exercise
+>多做写运动:
+>Do write more sports; Do write campaign; To do more writing exercise
+>睡前做些运动:
+>Bedtime take some exercise; Do some movement before sleeping
+
 
--- tranit.py
+++ tranit.py
@@ -1,58 +1,70 @@
 #!/usr/bin/env python
-# _*_ coding:utf8 _*_
+# _*_ coding: utf8 _*_
+
+'''This is a tiny application using for translate word or sentence from chinese to English , or from English to Chinese'''
 
 
-#This is a tiny application using for translate word or sentence from chinese to English , or from English to Chinese
-#
-#steps:
-#1 get the querry token from the command line
-#2  make the  full url 
-#3 requests the url and get the json
-#4 get the result from the json
+'''
+steps:
+1 get the querry token from the command line
+2 make the  full url
+3 requests the url and get the json
+4 get the result from the json
 
-##api  example:
-# http://fanyi.youdao.com/openapi.do?keyfrom=<keyfrom>&key=<key>&type=data&doctype=json&version=1.1&q=good
-# {
-#     "errorCode":0
-#     "query":"good",
-#     "translation":["好"], // 有道翻译
-#     "basic":{ // 有道词典-基本词典
-#         "phonetic":"gʊd"
-#         "uk-phonetic":"gʊd" //英式发音
-#         "us-phonetic":"ɡʊd" //美式发音
-#         "explains":[
-#             "好处",
-#             "好的"
-#             "好"
-#         ]
-#     },
-#     "web":[ // 有道词典-网络释义
-#         {
-#             "key":"good",
-#             "value":["良好","善","美好"]
-#         },
-#         {...}
-#     ]
-# }
+api  example:
+http://fanyi.youdao.com/openapi.do?keyfrom=<keyfrom>&key=<key>&type=data&doctype=json&version=1.1&q=good
+{
+    "errorCode":0
+    "query":"good",
+    "translation":["好"], // 有道翻译
+    "basic":{ // 有道词典-基本词典
+        "phonetic":"gʊd"
+        "uk-phonetic":"gʊd" //英式发音
+        "us-phonetic":"ɡʊd" //美式发音
+        "explains":[
+            "好处",
+            "好的"
+            "好"
+        ]
+    },
+    "web":[ // 有道词典-网络释义
+        {
+            "key":"good",
+            "value":["良好","善","美好"]
+        },
+        {...}
+    ]
+}
+
+'''
+
 
 import sys
 import requests
 import json
 
+KEYFILE = '/home/garrick/.youdao_key'
+
+def getkey():
+    with open(KEYFILE,'r') as f:
+        keyfrom = f.readline().split(':')[1].strip()
+        key = f.readline().split(':')[1].strip()
+    return keyfrom, key
+
 if __name__ == '__main__':
 	if len(sys.argv) <= 1:
 		print "Usage: tranit 'queryword or a sentence' (without quote)."
 		exit(1)
-
 	querry = " ".join(sys.argv[1:])
-	youdao = "http://fanyi.youdao.com/openapi.do?keyfrom=helloword159&key=540616135&type=data&doctype=json&version=1.1&q={}".format(querry)
+
+        keyfrom,key = getkey()
+	youdao = "http://fanyi.youdao.com/openapi.do?keyfrom={}&key={}&type=data&doctype=json&version=1.1&q={}".format(keyfrom, key, querry)
 	print querry
 
 	try:
 		res = requests.get(youdao);
 	except Exception,e:
 		print e
-	
 
 	jsondata = res.text
 	results = json.loads(jsondata)
